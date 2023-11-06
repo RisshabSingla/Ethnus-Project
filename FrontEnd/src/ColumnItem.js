ad0a55abaa5d093cd1d361945b0dedc14644a20f
Risshab Singla <risshab2singla@gmail.com>
2023-11-06 17:47:13 +0530

    Left Container Done + Right Container Lorem Ipsum

--- FrontEnd/src/ColumnItem.js
+++ FrontEnd/src/ColumnItem.js
@@ -1,19 +1,52 @@
-function ColumnItem({ imageSrc, ItemName }) {
+import { useEffect, useState } from "react";
+
+function ColumnItem({
+  id,
+  imageSrc,
+  ItemName,
+  onClickImage,
+  setClicked,
+  currentClicked,
+}) {
+  const originalClassName = "flex items-center w-full hover:bg-blue-800 p-3";
+  const originalTextColor = "text-orange-100";
+  const [svgSource, setSvgSource] = useState(imageSrc);
+  const [divClassName, setDivClassName] = useState(originalClassName);
+  const [textColor, setTextColor] = useState(originalTextColor);
+
   function onClick() {
+    setClicked(id);
     console.log(`${ItemName} Pressed`);
+    const className = divClassName + " bg-blue-800";
+    setDivClassName(className);
+    setSvgSource(onClickImage);
   }
+  useEffect(() => {
+    if (currentClicked !== id) {
+      setSvgSource(imageSrc);
+      setDivClassName(originalClassName);
+      setTextColor("text-orange-100");
+    } else {
+      setSvgSource(onClickImage);
+      const className = originalClassName + " bg-blue-800";
+      setDivClassName(className);
+      setTextColor("text-orange-500");
+    }
+  }, [currentClicked]);
 
   ItemName = ItemName.substr(0, 1).toUpperCase() + ItemName.substr(1);
   return (
-    <div className="flex items-center w-full  hover:bg-neutral-950 p-3">
+    <div className={divClassName}>
       <button
         onClick={onClick}
-        className="flex items-center w-full  hover:bg-neutral-950"
+        className="flex items-center w-full  hover:bg-blue-800"
       >
         <div className="flex items-center pl-3 mx-3 cursor-pointer">
-          <img width="35px" src={imageSrc} alt="" />
-          <div class="font-bold	 font-serif pl-3.5 text-lg text-slate-50">
-            {ItemName}
+          <img width="35px" src={svgSource} alt="" />
+          <div className="hidden min-[940px]:flex">
+            <div className={`font-bold font-serif pl-3.5 text-lg ${textColor}`}>
+              {ItemName}
+            </div>
           </div>
         </div>
       </button>
--- FrontEnd/src/LeftMain.js
+++ FrontEnd/src/LeftMain.js
@@ -1,11 +1,41 @@
 import ColumnItem from "./ColumnItem";
 
-function LeftMain() {
+function LeftMain({ clicked, setClickedItem }) {
   return (
     <div>
-      <ColumnItem imageSrc="./images/microphone/2.svg" ItemName="Podcasts" />
-      <ColumnItem imageSrc="./images/explore/1.svg" ItemName="Explore" />
-      <ColumnItem imageSrc="/images/favourites/1.svg" ItemName="Favourites" />
+      <ColumnItem
+        id="1"
+        imageSrc="./images/home/1.svg"
+        ItemName="Home"
+        onClickImage="./images/home/2.svg"
+        setClicked={setClickedItem}
+        currentClicked={clicked}
+      />
+      <ColumnItem
+        id="2"
+        imageSrc="./images/microphone/1.svg"
+        ItemName="Podcasts"
+        onClickImage="./images/microphone/2.svg"
+        setClicked={setClickedItem}
+        currentClicked={clicked}
+      />
+      <ColumnItem
+        id="3"
+        imageSrc="./images/explore/1.svg"
+        ItemName="Explore"
+        onClickImage="./images/explore/2.svg"
+        setClicked={setClickedItem}
+        currentClicked={clicked}
+      />
+
+      <ColumnItem
+        id="4"
+        imageSrc="/images/favourites/1.svg"
+        ItemName="Favourites"
+        onClickImage="./images/favourites/2.svg"
+        setClicked={setClickedItem}
+        currentClicked={clicked}
+      />
     </div>
   );
 }
