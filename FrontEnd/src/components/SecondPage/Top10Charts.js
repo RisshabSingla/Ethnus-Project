5cad464e06d12cc5dc95c1fef48eadccf99a93c4
Risshab Singla <risshab2singla@gmail.com>
2023-11-16 18:04:17 +0530

    Settings Page Left

--- FrontEnd/src/components/SecondPage/Top10Charts.js
+++ FrontEnd/src/components/SecondPage/Top10Charts.js
@@ -2,52 +2,6 @@ import Carousel from "react-elastic-carousel";
 import Podcast from "./Podcast";
 import "./Top10Charts.css";
 
-// const Alltracks = [
-//   {
-//     name: "Apna Bale Le",
-//     artist: "Trinix ft Rushawn",
-//     img: "https://ik.imagekit.io/exu18jfr4/img.jpg?updatedAt=1699356626542",
-//     podcast:
-//       "https://ik.imagekit.io/exu18jfr4/That_All_Men_Be_Saved_God_s_Desire_and_Our_Role_Part_166928.mp3?updatedAt=1699356823539",
-//   },
-//   {
-//     name: "Apna Bale Le",
-//     artist: "Trinix ft Rushawn",
-//     img: "./images/3.jpeg",
-//     podcast: "./music/1.mp3",
-//   },
-//   {
-//     name: "Chaand_Baaliyan",
-//     podcast: "./music/2.mp3",
-//     artist: "Trinix ft Rushawn",
-//     img: "",
-//   },
-//   {
-//     name: "Apna Bale Le",
-//     artist: "Trinix ft Rushawn",
-//     img: "./images/3.jpeg",
-//     podcast: "./music/2.mp3",
-//   },
-//   {
-//     name: "Chaand_Baaliyan",
-//     podcast: "./music/2.mp3",
-//     artist: "Trinix ft Rushawn",
-//     img: "./images/3.jpeg",
-//   },
-//   {
-//     name: "Apna Bale Le",
-//     artist: "Trinix ft Rushawn",
-//     img: "./images/3.jpeg",
-//     podcast: "./music/2.mp3",
-//   },
-//   {
-//     name: "Chaand_Baaliyan",
-//     podcast: "./music/2.mp3",
-//     artist: "Trinix ft Rushawn",
-//     img: "./images/3.jpeg",
-//   },
-// ];
-
 function Top10Charts({
   setCurrPlaying,
   currPlaying,
@@ -58,8 +12,6 @@ function Top10Charts({
   setUserFavourites,
   top10Podcasts = [],
 }) {
-  // sm:min-w-[300px] md:min-w-[500px] lg:min-w-[700px] xl:min-w-[1000px]
-  // sm:min-w-[300px] min-[730px]:min-w-[500px]  min-[920px]:min-w-[700px] xl:min-w-[1000px]
   return (
     <div className="rounded-2xl">
       <Carousel
@@ -86,19 +38,6 @@ function Top10Charts({
             setUserFavourites={setUserFavourites}
           />
         ))}
-
-        {/* <Podcast />
-        <Podcast />
-        <Podcast />
-        <Podcast />
-        <Podcast />
-        <Podcast />
-        <Podcast />
-        <Podcast />
-        <Podcast />
-        <Podcast />
-        <Podcast />
-        <Podcast /> */}
       </Carousel>
     </div>
   );
